aaca1feb6cf0f4c5741e2c4a208e41934fd05964
Your Name <you@example.com>
2021-04-14 21:17:28 +0800

    vue3

---
+++ vue3/h/src/components/init.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click="increment">{{ count }}</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent,Ref,ref } from '@vue/composition-api'
+
+export default defineComponent({
+    props:{
+        name:String
+    },
+    setup( props ){
+        const count:Ref<number> = ref(0); 
+        const increment = () => {
+            count.value++ ;
+        };
+        return {
+            count,
+            increment,
+        };
+    }
+})
+</script>
---
+++ vue3/h/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Init/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import Init from '@/components/init'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Init
+  },
+};
+</script>
