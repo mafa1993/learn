c5706d55131abd999858526bdaa5c46c09810ab7
Your Name <you@example.com>
2021-06-14 11:06:07 +0800

    fininshed

---
+++ mini-webpack/src/add.js
@@ -0,0 +1,8 @@
+// commen.js 规范，导出一个方法
+// exports.default = function(a,b) {
+//     return a+b;
+// }
+
+// es6 module方式
+
+export default (a,b) => a+b
--- mini-webpack/webpack.js
+++ mini-webpack/webpack.js
@@ -83,3 +83,22 @@ function getDeps(temp,{deps}) {  // 解构复制deps
         getDeps(temp,child)
     });
 }
+
+
+function bundle(file) {
+    const depsGraph = JSON.stringify(parseModules(file));
+
+    return `(function (graph){
+        function require(file) {
+            function absRequire(relPath){
+                return require(graph[file].deps[relPath])
+            }
+            var exports = {};
+            (function (require,exports,code){
+                eval(code);
+            })(absRequire,exports,graph[file].code)
+            return exports;
+        }
+        require('${file}')
+    })(${depsGraph})`
+}
